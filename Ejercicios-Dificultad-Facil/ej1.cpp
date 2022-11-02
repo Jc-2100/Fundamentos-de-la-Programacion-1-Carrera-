b996649538f9b479293da4c0e5d97fd1f36d9634
Jose Canto <47193615+Josecp77@users.noreply.github.com>
2022-11-02 21:59:35 +0100

    Update ej1.cpp

--- Ejercicios-Dificultad-Facil/ej1.cpp
+++ Ejercicios-Dificultad-Facil/ej1.cpp
@@ -1,3 +1,11 @@
+// Ejercicio más simple (y conocido) que se puede crear en programación, llamado "Hola Mundo"; así se vería en C++.
+// Usamos la librería estandar "iostream" que es la librería utilizada para operaciones de entrada/salida (Input/Output Stream).
+// Esta librería define el método "cout" usado en este programa denotado como "Console Out".
+
+// The simplest (and most well known) exercise that can be created in programming, it is called "Hello World"; this is what it would look like in C++.
+// We use the standard "iostream" library which is the library used for Input/Output Stream operations.
+// This library defines the "cout" method used in this program denoted as "Console Out".
+
 #include <iostream>
 using namespace std;
 
